3f69303e47737de155592679dadb52c1d8177394
sanketvagadiya <scvagadiya@gmail.com>
2019-10-03 18:34:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexample/RatingbarFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.fragmentexample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RatingbarFragment extends Fragment {
+
+
+    public RatingbarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_ratingbar, container, false);
+    }
+
+}
